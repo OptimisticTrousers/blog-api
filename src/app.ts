27d75a30f1e9a203c6d4b0a455c9de1847395281
OptimisticTrousers <tony.isern@yahoo.com>
2022-11-26 15:23:22 -0500

    Setting up app.js boilerplate

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
 import express, { Application, Request, Response, NextFunction } from "express";
+import mongoose, { ConnectOptions } from "mongoose";
+import logger from "morgan";
+import cors from "cors";
 
 const app: Application = express();
 
-const add = (a: number, b: number): number => a + b;
+const mongoDB = process.env.DB_STRING;
+mongoose.connect(mongoDB!, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+} as ConnectOptions);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  console.log(add(5, 5))
-  res.send("Hello");
-});
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(cors());
 
-app.listen(3000, () => console.log("Server running"));
+// routes
+app.use("/blogs");
+
+app.listen(process.env.PORT, () => console.log("Server running"));
