cbaa05132055984ec7afef512aa81f78e36c10fa
OptimisticTrousers <tony.isern@yahoo.com>
2022-11-26 16:13:09 -0500

    Handling routes

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -1,9 +1,17 @@
-import { Router, Response, Request, NextFunction} from "express";
+import { Router } from "express";
+import post_controller from "../controllers/blogController";
 
 const router: Router = Router();
 
-router.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.json()
-})
+router
+  .route("/")
+  .get(post_controller.post_list)
+  .post(post_controller.post_create_post);
+
+router
+  .route("/:postId")
+  .get(post_controller.post_detail)
+  .put(post_controller.post_update_put)
+  .delete(post_controller.post_update_delete);
 
 export default router;
