feb620644c2a426b26a06b2ecf679093f7781079
OptimisticTrousers <tony.isern1@gmail.com>
2022-12-24 17:18:32 -0500

    New changes

--- src/controllers/commentController.ts
+++ src/controllers/commentController.ts
@@ -5,17 +5,21 @@ import { body, validationResult } from "express-validator";
 // Handle comment create on POST
 const comment_create_post = [
   // Validate and sanitize data
-  body("name", "Please enter a name").trim().isLength({ min: 1 }),
-  body("email", "Please enter an email").trim().isEmail(),
+  body("name", "Please enter a name").trim().isLength({ min: 1 }).escape(),
+  body("email", "Please enter an email").trim().isEmail().escape(),
   body("contentHtml", "Please enter content").trim().isLength({ min: 1 }),
   // Process request after validation and sanitization
   (req: Request, res: Response, next: NextFunction) => {
     // Extract the validation errors from a request
     const errors = validationResult(req);
 
+    console.log(req.body)
+
     // Create a Post object with trimmed data
     const newComment = new Comment({
       name: req.body.name,
+      email: req.body.email,
+      postId: req.body.postId,
       contentHtml: req.body.contentHtml,
     });
 
